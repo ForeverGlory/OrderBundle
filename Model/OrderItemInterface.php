0afd53a9750889451a9496f31f839d78d63b4ee4
ForeverGlory <foreverglory@qq.com>
2017-01-09 23:01:22 +0800

    OrderItemInterface

---
+++ Model/OrderItemInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\OrderBundle\Model;
+
+/**
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+interface OrderItemInterface
+{
+    //put your code here
+}
